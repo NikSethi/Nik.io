bf5d2b0639ae2d9aeea653ef571ebe880895e443
NikSethi <nikhilsethi108@gmail.com>
2014-06-05 19:33:36 -0400

    Reverse chronological

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'articles/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :articles
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'articles#index'
 
   resources :articles do
     resources :comments
